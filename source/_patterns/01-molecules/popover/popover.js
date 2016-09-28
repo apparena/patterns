810360b5578097ee53aaf5571f4f44d11b8525da
Valentin Klein <v.klein@iconsultants.eu>
2016-09-28 11:45:33 +0200

    add popover

--- source/_patterns/01-molecules/popover/popover.js
+++ source/_patterns/01-molecules/popover/popover.js
@@ -7,69 +7,44 @@ import styles from "./popover.scss";
 
 export default class PopOver extends ReactComponent {
     static propTypes = {
-        children: PropTypes.node.isRequired,
-        header: PropTypes.string.isRequired,
-        content: PropTypes.string.isRequired,
+        children: PropTypes.element.isRequired,
         className: PropTypes.string,
-        fixed: PropTypes.bool,
+        title: PropTypes.string,
+        content: PropTypes.element.isRequired,
+        open: PropTypes.bool,
         positioning: PropTypes.oneOf([
             'top',
             'bottom',
             'left',
             'right'
         ]),
+        zIndex: PropTypes.number
     };
 
     static defaultProps = {
-        positioning: "top"
+        positioning: "top",
+        open: false,
+        zIndex: 1060
     };
 
     getInitState() {
         return {
-            showing: false
+            showing: this.props.open
         };
     }
 
-    showPopOver() {
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.open !== this.state.open) {
+            this.setState({
+                open: nextProps.open
+            });
+        }
+    }
+
+    toggleTooltip() {
         this.setState({
-            showing: true
+            open: !this.state.open
         });
-    }
-
-    hidePopOver() {
-        this.setState({
-            showing: false
-        });
-    }
-
-    onMouseEnter() {
-        this.timeout = setTimeout(::this.showPopOver, 500);
-    }
-
-    onMouseLeave() {
-        clearTimeout(this.timeout);
-        this.hidePopOver();
-    }
-
-    onMouseClick() {
-        this.setState({
-            showing: !this.state.showing
-        });
-    }
-
-    render() {
-        const {className} = this.props;
-
-        return (
-            <div
-                className={className || styles.root}
-                onClick={::this.onMouseClick}
-                ref={c => {this._handler = c}}
-            >
-                {this.props.children}
-                {this.renderPopOver()}
-            </div>
-        );
     }
 
     calculateVerticalOffset() {
@@ -83,9 +58,7 @@ export default class PopOver extends ReactComponent {
     }
 
     renderPopOver() {
-        if (this.state.showing) {
-            const {header, content} = this.props;
-
+        if (this.state.open) {
             return (
                 <Portal>
                     <Stick
@@ -93,20 +66,34 @@ export default class PopOver extends ReactComponent {
                         positioning={this.props.positioning}
                         verticalOffset={this.calculateVerticalOffset()}
                         horizontalOffset={this.calculateHorizontalOffset()}
+                        zIndex={this.props.zIndex}
                     >
-                        <div className={styles.popover}>
-                            <div className={cx(styles.popoverShadowArrow, styles[this.props.positioning + '-shadow'])}></div>
-                            <div className={cx(styles.popoverArrow, styles[this.props.positioning])}></div>
-                            <h3 className={styles['popover-title']}>
-                                {header}
-                            </h3>
-                            <div className={styles['popover-content']}>
-                                {content}
+                        <div className={cx(styles.popover, styles[`popover-${this.props.positioning}`])}>
+                            <div className={styles["popover-arrow"]}></div>
+                            {this.props.title && <h3 className={styles["popover-title"]}>{this.props.title}</h3>}
+                            <div className={styles["popover-content"]}>
+                                {this.props.content}
                             </div>
                         </div>
                     </Stick>
                 </Portal>
             );
         }
+    }
+
+    render() {
+        const {className} = this.props;
+        return (
+            <div
+                className={className || styles.root}
+                onClick={::this.toggleTooltip}
+                ref={c => {
+                    this._handler = c
+                }}
+            >
+                {this.props.children}
+                {this.renderPopOver()}
+            </div>
+        );
     }
 }
