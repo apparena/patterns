9ff586f8c18dcd42b5856d9cbde8370eb3b28e18
Valentin Klein <v.klein@iconsultants.eu>
2016-10-21 15:35:29 +0200

    npm version 1.0.37

--- source/_patterns/index.js
+++ source/_patterns/index.js
@@ -1,80 +1,82 @@
-import ReactComponent from "./react-utils/component";
-import Animate from "./react-utils/animate";
-import Portal from "./react-utils/portal";
-import Stick from "./react-utils/stick";
-import Scrollable from "./react-utils/scrollable";
-import Button from "./00-atoms/button/button";
-import Checkbox from "./00-atoms/forms/checkbox";
-import Col from "./00-atoms/grid/col";
-import ProgressTrackerStep from "./00-atoms/progress-tracker-step/progress-tracker-step";
-import Icon from "./00-atoms/icons/icons";
-import Input from "./00-atoms/forms/input";
-import Row from "./00-atoms/grid/row";
-import Spinner from "./00-atoms/spinner/spinner";
-import Select from "./00-atoms/forms/select-menu";
 import Alert from "./01-molecules/alert/alert";
+import Animate from "./react-utils/animate";
 import BreadCrumb from "./01-molecules/breadcrumb/breadcrumb";
+import Button from "./00-atoms/button/button";
 import ButtonGroup from "./01-molecules/button-group/button-group";
 import Card from "./01-molecules/card/card";
+import CardsList from "./02-organisms/lists/cards-list";
 import Chart from "./01-molecules/chart/chart";
+import Checkbox from "./00-atoms/forms/checkbox";
+import Col from "./00-atoms/grid/col";
 import Dropdown from "./01-molecules/dropdown/dropdown";
 import FilterBar from "./01-molecules/01-manager/filter-bar/filter-bar";
+import Icon from "./00-atoms/icons/icons";
+import Input from "./00-atoms/forms/input";
 import InputGroup from "./01-molecules/input-group/input-group";
 import Jumbotron from "./01-molecules/jumbotron/jumbotron";
+import ListGroup from "./01-molecules/list-group/list-group";
 import Modal from "./01-molecules/modal/modal";
-import Navbar from "./01-molecules/navbar/navbar";
 import Nav from "./01-molecules/navs/nav";
+import Navbar from "./01-molecules/navbar/navbar";
 import NavItem from "./01-molecules/navs/nav-item";
 import NavSecondary from "./02-organisms/navigation/nav-secondary";
 import NavSecondaryGroup from "./02-organisms/navigation/nav-secondary-group";
 import Notification from "./01-molecules/notification/notification";
 import Popover from "./01-molecules/popover/popover";
-import Tooltip from "./01-molecules/tooltip/tooltip";
+import Portal from "./react-utils/portal";
+import ProgressBar from "./01-molecules/progress-bar/progress-bar";
 import ProgressTracker from "./01-molecules/progress-tracker/progress-tracker";
-import ListGroup from "./01-molecules/list-group/list-group";
-import CardsList from "./02-organisms/lists/cards-list";
-import TableList from "./02-organisms/lists/table-list";
+import ProgressTrackerStep from "./00-atoms/progress-tracker-step/progress-tracker-step";
+import ReactComponent from "./react-utils/component";
+import Row from "./00-atoms/grid/row";
+import Scrollable from "./react-utils/scrollable";
+import Select from "./00-atoms/forms/select-menu";
+import Spinner from "./00-atoms/spinner/spinner";
+import Stick from "./react-utils/stick";
 import Table from "./00-atoms/table/table";
+import TableList from "./02-organisms/lists/table-list";
 import Tag from "./00-atoms/tag/tag";
+import Tooltip from "./01-molecules/tooltip/tooltip";
 
 
 export {
-    ReactComponent,
-    Animate,
-    Portal,
-    Stick,
-    Scrollable,
-    Button,
-    Checkbox,
-    Col,
-    ProgressTrackerStep,
-    Icon,
-    Input,
-    Row,
-    Spinner,
-    Select,
     Alert,
+    Animate,
     BreadCrumb,
+    Button,
     ButtonGroup,
     Card,
+    CardsList,
     Chart,
+    Checkbox,
+    Col,
     Dropdown,
     FilterBar,
+    Icon,
+    Input,
     InputGroup,
     Jumbotron,
+    ListGroup,
     Modal,
-    Navbar,
     Nav,
+    Navbar,
     NavItem,
     NavSecondary,
     NavSecondaryGroup,
     Notification,
     Popover,
-    Tooltip,
+    Portal,
+    ProgressBar,
     ProgressTracker,
-    ListGroup,
-    CardsList,
-    TableList,
+    ProgressTrackerStep,
+    ReactComponent,
+    Row,
+    Scrollable,
+    Select,
+    Spinner,
+    Stick,
     Table,
+    TableList,
     Tag,
+    Tooltip,
 }
