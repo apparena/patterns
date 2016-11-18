aaaece9f252547db67ada5035f19a96b2b38c877
Valentin Klein <v.klein@iconsultants.eu>
2016-11-18 11:53:03 +0100

    refactor modal component (split to modal-header, modal-footer, modal-body) to make it more reusable

--- source/_patterns/01-molecules/forms/form-group.js
+++ source/_patterns/01-molecules/forms/form-group.js
@@ -1,28 +1,27 @@
 import React, {PropTypes} from "react";
 import cx from "classnames";
 import styles from "./form-group.scss";
-import Tooltip from "../tooltip/tooltip";
 
-function FormGroup({className, htmlFor, label, validationFeedback, children, validationState, ...props}) {
-    let childrenProps= children.props;
-    props.className = cx(styles["form-group"], className);
+function FormGroup({className, htmlFor, label, children, validationState, validationFeedback, ...props}) {
+    props.className = cx(styles["form-group"], validationState && styles[`has-${validationState}`], className);
 
-    if (validationState) {
-        childrenProps.className = cx(childrenProps.className, styles[`has-${validationState}`]);
-    }
-
-    if (validationFeedback) {
-        children = (
-            <Tooltip label={validationFeedback}>
-                {children}
-            </Tooltip>
-        )
-    }
+    children = React.Children.map(children, (child) => {
+        if (child.type.name === "Input") {
+            return React.cloneElement(child, {
+                className: validationState ? cx(child.props.className, styles[`form-control-${validationState}`]) : cx(child.props.className)
+            });
+        } else {
+            return React.cloneElement(child);
+        }
+    });
 
     return (
         <div {...props} >
-            <label htmlFor={htmlFor}>{label}</label>
+            <label className={styles["form-control-label"]} htmlFor={htmlFor}>{label}</label>
             {children}
+            {(validationState && validationFeedback) &&
+            <div className={styles["form-control-feedback"]}>{validationFeedback}</div>
+            }
         </div>
     );
 }
@@ -33,7 +32,8 @@ FormGroup.propTypes = {
     htmlFor: PropTypes.string,
     id: PropTypes.string,
     label: PropTypes.string,
-    validationState: PropTypes.string,
+    validationFeedback: PropTypes.string,
+    validationState: PropTypes.oneOf(["danger", "success", "warning"]),
 };
 
 export default FormGroup;
