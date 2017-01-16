ddbf63c32f637e15a03b11310495cd60ef7a527a
Marius <m.brandt@app-arena.com>
2017-01-16 16:59:59 +0100

    Added custom checkboxes to price table

--- source/_patterns/02-organisms/price-table/custom-package-creator.js
+++ source/_patterns/02-organisms/price-table/custom-package-creator.js
@@ -16,6 +16,8 @@ export default class CustomPackageCreator extends ReactComponent {
         return {
             dropdown1Value: "",
             dropdown2Value: "",
+            tickedCheckboxes: [],
+            price: 0,
         };
     }
 
@@ -31,23 +33,61 @@ export default class CustomPackageCreator extends ReactComponent {
         });
     }
 
+    onCBChange(e) {
+        if (e.target.checked) {
+            this.setState({
+                tickedCheckboxes: [...this.state.tickedCheckboxes, parseInt(e.target.id, 10)],
+                price: this.state.price + parseInt(e.target.value, 10)
+            });
+        } else {
+            let tmp = this.state.tickedCheckboxes;
+            let idx = tmp.indexOf(parseInt(e.target.id, 10));
+            tmp.splice(idx, 1);
+            this.setState({
+                tickedCheckboxes: tmp,
+                price: this.state.price - parseInt(e.target.value, 10)
+            });
+        }
+    }
+
+    renderCheckboxList(dataSource, onChange) {
+        return (
+            <ul className={styles.checkboxes}>
+                {dataSource.map((e, i) => {
+                    return (
+                        <li key={i}>
+                            {!this.state.tickedCheckboxes.includes(i) &&
+                                <span className={cx("fa fa-times-circle-o", styles.cbUnchecked)} />
+                            }
+                            {this.state.tickedCheckboxes.includes(i) &&
+                                <span className={cx("fa fa-check-circle-o", styles.cbChecked)} />
+                            }
+                            <input id={i} type="checkbox" value={e.price} onChange={onChange}/>
+                            <label htmlFor={i}>{e.label}</label>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
     render(){
         return (
             <div>
                 {this.props.visible && (
                     <div className={styles.packageCreator}>
-                        <Row>
+                        <Row className={styles.headerRow}>
                             <Col md="3" className={styles.titleLeft}>
                                 <p>IHR INDIVIDUELLES PAKET</p>
                             </Col>
                             <Col md="3" mdOffset={6} className={styles.sumContainer}>
                                 <span id="sum" className={styles.sumDisplay}>
-                                    <sup>€{this.state.sum}</sup>
+                                    <sup>€{this.state.price}</sup>
                                 </span>
                             </Col>
                         </Row>
                         <Row>
-                            <Col md="4">
+                            <Col md="4" className={styles.dropdownContainer}>
                                 <SelectMenu defaultValue={this.state.dropdown1Value}
                                             options={data.custom.dropdown1} onChange={::this.onDD1Change}
                                 />
@@ -56,10 +96,10 @@ export default class CustomPackageCreator extends ReactComponent {
                                 />
                             </Col>
                             <Col md="4">
-
+                                {this.renderCheckboxList(data.custom.checkboxes, ::this.onCBChange)}
                             </Col>
                             <Col md="4">
-
+                                {this.renderCheckboxList(data.custom.topRightCheckboxes, ::this.onCBChange)}
                             </Col>
                         </Row>
                     </div>
