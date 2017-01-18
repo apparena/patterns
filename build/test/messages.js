a78feed5b139204aac864767ad41fb4144b7fee9
Marius <m.brandt@app-arena.com>
2017-01-18 15:34:32 +0100

    Added missing translation file

--- build/test/messages.js
+++ build/test/messages.js
@@ -2,7 +2,24 @@ export default {
     de: {
         priceTable: {
             leftText: "Einzelpakete",
-            rightText: "Flatratepakete"
+            rightText: "Flatratepakete",
+            header: "Preise"
+        },
+        priceTableElement: {
+            popular: "BELIEBT",
+        },
+        customPackage: {
+            info: "Nichts passendes dabei?<br />Kein Problem, stellen Sie hier Ihr eigenes Paket zusammen!",
+            buttonPrompt: "Eigenes Paket erstellen",
+            removeLanguage: "Entfernen",
+            addLanguagePrompt: "weitere Sprache buchen (ab 99€)",
+            header: "IHR INDIVIDUELLES PAKET",
+            step1Hint: "Im ersten Schritt stellen Sie hier die allgemeinen Daten Ihres Paketes zusammen.",
+            step2Hint: "Im nächsten Schritt können Sie angeben, wie viele Servicestunden Sie benötigen.",
+            step3Hint: "Wähle über welche Kanäle die App veröffentlicht werden soll.",
+            dropdownError: "Bitte wählen Sie eine Option aus",
+            rightCenterHeader: "Domain der Kampagnenseite",
+            purchasePrompt: "Kaufen"
         }
     }
 }
