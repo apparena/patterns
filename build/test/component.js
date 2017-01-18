50074318b7da71b671981bcaca6b769c9092aa8a
Valentin Klein <v.klein@iconsultants.eu>
2017-01-18 12:36:31 +0100

    add messages.js to test folder for inserting translations in react components.

--- build/test/component.js
+++ build/test/component.js
@@ -1,6 +1,15 @@
 import React from "react";
 import {render} from "react-dom";
 import {ReactComponent, PriceTable} from "../../source/_patterns/index";
+import i18n from "i18n-js";
+import translations from "./messages";
+
+//Setting up i18n
+i18n.defaultLocale = "de";
+i18n.locale = "de";
+i18n.fallbacks = true;
+i18n.translations = translations;
+
 
 class Test extends ReactComponent {
     getInitState() {
