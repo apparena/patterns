380a6acf6029a8297291df09a7accca6f03e046e
Sebastian Buckpesch <s.buckpesch@app-arena.com>
2017-02-09 10:55:07 +0100

    Add icon script to styleguide to generate the webfonts using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,28 @@ module.exports = function (grunt) {
                 files: {
                     'public/css/app-arena-styles.css': 'source/css/app-arena-styles.scss',
                     'source/css/app-arena-styles.css': 'source/css/app-arena-styles.scss',
-                    //'public/css/easycontest-styles.css': 'source/css/easycontest-styles.scss',
-                    //'source/css/easycontest-styles.css': 'source/css/easycontest-styles.scss',
-                    //'public/css/website-wordpress-styles.css': 'source/css/website-wordpress-styles.scss',
-                    //'source/css/website-wordpress-styles.css': 'source/css/website-wordpress-styles.scss',
-                    //'public/css/bwgv-styles.css': 'source/css/bwgv-styles.scss',
-                    //'source/css/bwgv-styles.css': 'source/css/bwgv-styles.scss',
                 }
+            }
+        },
+        run: {
+            options: {
+            },
+            // Runt the icon generator https://github.com/Workshape/icon-font-generator
+            icon_generator: {
+                cmd: 'icon-font-generator',
+                args: [
+                    'source/icons/svg/*.svg',
+                    '--out=source/fonts',
+                    '--name=icons',
+                    '--prefix=aa-icon',
+                    '--normalize=true',
+                    //'--tag=i',
+                ]
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-run');
     grunt.loadTasks('build/scripts/grunt');
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'run']);
 };
