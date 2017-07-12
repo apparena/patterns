5cb8f35cefc5f284388dee9b06a19e130aff0340
Valentin Klein <v.klein@iconsultants.eu>
2017-07-12 18:29:48 +0200

    add build.sh and deploy.sh

---
+++ build/generator/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Init env variables
+export RootPath=$(pwd)
+
+echo "$(date): Start building"
+echo "RootPath: ${RootPath}"
+
+echo "$(date): Start Yarn install & build"
+yarn
+yarn build:docs
+echo "$(date): End Yarn install & build"
+
+# Compress source code to generate the artifacts
+echo "$(date): Start building artifacts"
+cd build/generator/frontend/src/
+tar -cvzf ../../../../artifact.tar.gz . --exclude=artifact.tar.gz --warning=no-file-changed > /dev/null
+echo "$(date): End building artifacts"
