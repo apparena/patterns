e6bc81808b95fc6380d10ea7f2d875eca5937cee
therweg <t.herweg@app-arena.com>
2017-08-01 12:50:13 +0200

    Added AVA test for button

--- source/_patterns/00-atoms/button/__tests__/button.spec.js
+++ source/_patterns/00-atoms/button/__tests__/button.spec.js
@@ -2,58 +2,40 @@
 import React from "react";
 import {shallow, mount} from "enzyme";
 import {spy} from "sinon";
-import test from 'ava';
-
+import {test} from 'ava';
+import {expect} from 'chai';
 import Button from "../button";
 
-test('renders a button'), t => {
-    const wrapper = shallow(<Button />)
+test('renders a button', t =>{
+    const wrapper = shallow(<Button />);
     t.is(wrapper.is('button'), true);
-}
+});
 
-/*test('<Button />', () => {
+test('renders children', t => {
+    const wrapper = shallow(
+        <Button><p>Hello World</p></Button>
+    )
+    t.is(wrapper.is('button'), true);
+    expect(wrapper.find('p')).to.have.length(1);
+    t.is(wrapper.find('p').contains('Hello World'), true);
+});
 
-    it('renders a button', () => {
-        const wrapper = shallow(
-            <Button />
-        );
-        expect(wrapper.is('button')).to.equal(true);
-    });
+test('reacts to onClick', t => {
+    var onClick = spy();
+    const wrapper = mount(
+        <Button onClick={onClick}><p>Hello World</p></Button>
+    );
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+    wrapper.find('button').simulate('click');
+    t.is(onClick.calledThrice, true);
+});
 
-    it('renders text', () => {
-        const wrapper = shallow(
-            <Button text="aloha" />
-        );
-        expect(wrapper.is('button')).to.equal(true);
-        expect(wrapper.contains('aloha')).to.equal(true);
-    });
-
-    it('renders children', () => {
-        const wrapper = shallow(
-            <Button><p>Hello world</p></Button>
-        );
-        expect(wrapper.is('button')).to.equal(true);
-        expect(wrapper.find('p')).to.have.length(1);
-        expect(wrapper.find('p').contains('Hello world')).to.equal(true);
-    });
-
-    it('reacts to onClick', () => {
-        const onClick = spy();
-        const wrapper = mount(
-            <Button onClickMethod={onClick}><p>Hello world</p></Button>
-        );
-        wrapper.find('button').simulate('click');
-        wrapper.find('button').simulate('click');
-        wrapper.find('button').simulate('click');
-        expect(onClick).to.have.property('callCount', 3);
-    });
-
-    it('reacts to onClick with identifier', () => {
-        const onClick = spy();
-        const wrapper = mount(
-            <Button identifier="custom-button-id" onClickMethod={onClick}><p>Hello world</p></Button>
-        );
-        wrapper.find('button').simulate('click');
-        expect(onClick.calledWith("custom-button-id")).to.equal(true);
-    });
-});*/
+test.failing('reacts to onClick with identifier', t => {
+    const onClick = spy();
+    const wrapper = mount(
+        <Button identifier="custom-button-id" onClick={onClick}><p>Hellow World</p></Button>
+    );
+    wrapper.find('button').simulate('click');
+    t.is(onClick.calledWith("custom-button-id"), true);
+})
