414e0734b774115eebb77b5087ae8b87c4609db2
Valentin Klein <v.klein@iconsultants.eu>
2017-08-04 10:40:40 +0200

    enable scss styling in ava tests

--- build/webpack/webpack.config.ava.js
+++ build/webpack/webpack.config.ava.js
@@ -1,18 +1,9 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-let extractComponentCSS = new ExtractTextPlugin({
-    filename: `styles/[name].css`,
-    allChunks: true
-});
-
 /// / webpack.config.js css-modules loader example
 module.exports = {
     output: {
         // YOU NEED TO SET libraryTarget: 'commonjs2'
         libraryTarget: 'commonjs2'
     },
-    plugins: [
-        extractComponentCSS // eslint-disable-line
-    ],
     resolve: {
         extensions: ['.js', '.jsx', '.es6'],
         modules: [
@@ -57,38 +48,7 @@ module.exports = {
                         name: `vendor/[name].[ext]`,
                     }
                 }],
-            },
-            /*{
-                test: /\.scss$/,
-                use: extractComponentCSS.extract({
-                    fallback: 'style-loader',
-                    publicPath: '../',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                                importLoaders: 1,
-                                localIdentName: '[local]_[hash:base64:5]',
-                                camelCase: true,
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'resolve-url-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
-            }*/
+            }
         ]
     }
 };
