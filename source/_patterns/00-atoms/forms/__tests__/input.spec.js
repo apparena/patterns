9847adc05ba94c58a0cab46fca64faf6fb075b69
Valentin Klein <v.klein@iconsultants.eu>
2017-08-04 09:15:02 +0200

    update readme

--- source/_patterns/00-atoms/forms/__tests__/input.spec.js
+++ source/_patterns/00-atoms/forms/__tests__/input.spec.js
@@ -1,5 +1,4 @@
 /* eslint-env mocha */
-<<<<<<< HEAD
 import React from "react";
 import {mount} from "enzyme";
 import {spy} from "sinon";
@@ -22,29 +21,3 @@ test('Input accepts custom functions', t => {
         .simulate('change', {target: {value:'My new value'}});
     t.is(onChange.calledTwice, true);
 });
-=======
-import React from 'react';
-import {test} from 'ava';
-import {mount} from 'enzyme';
-import {expect} from 'chai';
-import {spy} from 'sinon';
-import Input from '../input';
-
-test('is focused by default', () => {
-    const onChange = spy();
-    const wrapper = mount(
-        <Input onChange={onChange}/>
-    );
-    expect(wrapper.find('input').node).to.equal(document.activeElement);
-});
-
-test('accepts custom functions', () => {
-    const onChange = spy();
-    const wrapper = mount(
-        <Input onChange={onChange}/>
-    );
-    wrapper.find('input').simulate('change', {target: {value: 'My old value'}})
-        .simulate('change', {target: {value: 'My new value'}});
-    expect(onChange).to.have.property('callCount', 2);
-});
->>>>>>> 3c3bc4e8471f121b9a788fcf2d6b5090fe331479
