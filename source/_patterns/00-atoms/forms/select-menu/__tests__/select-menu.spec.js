a56ade75bfe5c674f5a251de730ec2dbe0dbfb62
Valentin Klein <v.klein@iconsultants.eu>
2017-08-22 10:35:46 +0200

    change travis config

--- source/_patterns/00-atoms/forms/select-menu/__tests__/select-menu.spec.js
+++ source/_patterns/00-atoms/forms/select-menu/__tests__/select-menu.spec.js
@@ -1,32 +1,32 @@
 /* eslint-env mocha */
-import React from "react";
-import {shallow, mount} from "enzyme";
-import {expect} from "chai";
-import {spy} from "sinon";
-import {test} from "ava";
-import SelectMenu from "../index";
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {expect} from 'chai';
+import {spy} from 'sinon';
+import {test} from 'ava';
+import SelectMenu from '../index';
 
-test.failing('Select Menu renders elements correctly', (t) =>{
+test.failing('Select Menu renders elements correctly', (t) => {
     const onChange = spy();
     const wrapper = shallow(
-        <SelectMenu defaultValue="" options={[1,2,3,4,5]} onChange={onChange} />
+        <SelectMenu defaultValue="" options={[1, 2, 3, 4, 5]} onChange={onChange}/>
     );
-    t.is(wrapper.find('select'), 1)
-    t.is(wrapper.find('optgroup').length, 1)
-    t.is(wrapper.find('option').length, 5)
-    
-})
+    t.is(wrapper.find('select'), 1);
+    t.is(wrapper.find('optgroup').length, 1);
+    t.is(wrapper.find('option').length, 5);
+
+});
 
 test.failing('handles change correctly', (t) => {
     const onChange = spy();
     const wrapper = mount(
-        <SelectMenu id="custom-select-menu" options={[1,2,3,4,5]} onChange={onChange} />
+        <SelectMenu id="custom-select-menu" options={[1, 2, 3, 4, 5]} onChange={onChange}/>
     );
     wrapper.find('select').simulate('change', {target: {value: 4}});
 
     t.is(onChange.calledOnce, true);
-    t.is(onChange.firstCall.args[0]['target']['value'], 4)
-    t.is(wrapper.find('select')).length, 1 );
-    t.is(wrapper.find('optgroup')).length, 1 );
-    t.is(wrapper.find('option')).length, 5 );
-})
+    t.is(onChange.firstCall.args[0]['target']['value'], 4);
+    t.is(wrapper.find('select').length, 1);
+    t.is(wrapper.find('optgroup').length, 1);
+    t.is(wrapper.find('option').length, 5);
+});
