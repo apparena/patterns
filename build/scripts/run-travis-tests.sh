da330247bf45aa6f01b0155abb25b70e5387685b
Marius <m.brandt@app-arena.com>
2017-11-17 13:50:07 +0100

    added generator tests to travis test script

--- build/scripts/run-travis-tests.sh
+++ build/scripts/run-travis-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 yarn run test
+yarn run test:generator
 yarn run test:coverage
 cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
 cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
