15728f042dbbe0423ccceaee5ae6629ee1958149
Sebastian Buckpesch <s.buckpesch@app-arena.com>
2018-02-16 10:03:04 +0100

    Add documents and presentation templates

--- source/pages/brand/index.js
+++ source/pages/brand/index.js
@@ -1,17 +1,17 @@
-import * as P01Personality from './personality';
-import * as P02Colors from './colors';
-import * as P03Documents from './documents';
-import * as P04Icons from './icons';
-import * as P05Logo from './logos';
-import * as P06Presentations from './presentations';
-import * as P07Typography from './typography';
+import * as AppArenaPersonality from './personality';
+import * as Colors from './colors';
+import * as Documents from './documents';
+import * as Icons from './icons';
+import * as Logo from './logos';
+import * as Presentations from './presentations';
+import * as Typography from './typography';
 
 export {
-    P01Personality,
-    P02Colors,
-    P03Documents,
-    P04Icons,
-    P05Logo,
-    P06Presentations,
-    P07Typography
+    AppArenaPersonality,
+    Colors,
+    Documents,
+    Icons,
+    Logo,
+    Presentations,
+    Typography
 };
