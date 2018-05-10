73f34fce6b98807d030432bf874a46e2d2cbb60e
Sebastian Buckpesch <s.buckpesch@app-arena.com>
2018-05-10 16:01:26 +0200

    [patch] Update nav component

--- source/patterns/01-molecules/navs/nav/docs/nav.example.js
+++ source/patterns/01-molecules/navs/nav/docs/nav.example.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import {Nav, NavItem} from 'apparena-patterns-react';
+import { Nav, NavItem } from 'apparena-patterns-react';
 
-export default function NavExample() {
+export default class NavExample extends React.Component{
+
+  state = {
+    activeItem: "nav1"
+  };
+
+  handleChange(e){
+    this.setState({
+      activeItem: e.target.id
+    });
+  }
+
+  render() {
     return (
-        <Nav tabs>
-            <NavItem>
-                <a>Item 1</a>
-            </NavItem>
-            <NavItem>
-                <a>Numero 2</a>
-            </NavItem>
-            <NavItem>
-                <a>Part 3</a>
-            </NavItem>
-        </Nav>
+      <Nav tabs>
+        <NavItem active={this.state.activeItem === "nav1"}>
+          <a id="nav1" onClick={this.handleChange.bind(this)}>Active item</a>
+        </NavItem>
+        <NavItem active={this.state.activeItem === "nav2"}>
+          <a id="nav2" onClick={this.handleChange.bind(this)}>Default item</a>
+        </NavItem>
+        <NavItem active={this.state.activeItem === "nav3"}>
+          <a id="nav3" onClick={this.handleChange.bind(this)}>Default item</a>
+        </NavItem>
+      </Nav>
     );
+  }
+
 }
