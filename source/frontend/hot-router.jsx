69582b6f0c789bdcbb2a89ea6a7faccb4e934b74
Sebastian Buckpesch <s.buckpesch@app-arena.com>
2018-05-10 09:54:28 +0200

    [patch] Update button group styling to affect child buttons

--- source/frontend/hot-router.jsx
+++ source/frontend/hot-router.jsx
@@ -1,16 +1,15 @@
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader';
 import Home from './home';
-import React from "react";
-import {HashRouter, Route} from "react-router-dom";
-import {hot} from 'react-hot-loader';
 
 const HotRouter = () => (
-    <HashRouter>
-        <div>
-            <Route path="/" component={Home}/>
-        </div>
-    </HashRouter>
+  <HashRouter>
+    <div>
+      <Route path="/" component={Home} />
+    </div>
+  </HashRouter>
 );
 
 export default hot(module)(HotRouter);
-
 
