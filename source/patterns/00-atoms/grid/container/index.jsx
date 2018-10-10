741c2c58fc6507497fbf49f1432ff883b67697dd
Thomas Herweg <t.herweg@app-arena.com>
2018-10-10 17:05:43 +0200

    Added deprecation warning in the console

--- source/patterns/00-atoms/grid/container/index.jsx
+++ source/patterns/00-atoms/grid/container/index.jsx
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from '../grid.scss';
 
+/**
+ * @deprecated
+ * @param className
+ * @param fluid
+ * @param children
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
 function Container({
   className, fluid, children, ...props
 }) {
   props.className = cx(fluid ? styles['container-fluid'] : styles.container, className);
-
+  console.warn('The Container component will be removed in the next major release. Replace this component with the Bootstrap container class');
   return (
     <div {...props}>
       {children}
